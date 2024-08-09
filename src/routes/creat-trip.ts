9be7efd071892ecbc1ba18729be8cfc77e6f6410
Vinicius-Coradini <vinicius.coradini@outlook.com>
2024-08-09 10:02:46 -0300

    Participants created

--- src/routes/creat-trip.ts
+++ src/routes/creat-trip.ts
@@ -16,11 +16,11 @@ export async function createTrip(app: FastifyInstance) {
                 ends_at: z.coerce.date(),
                 owner_name: z.string(),
                 owner_email: z.string().email(),
-
+                emails_to_invite: z.array(z.string().email())
             })
         }
     }, async (request) => {
-        const { destination, starts_at, ends_at, owner_name, owner_email } = request.body
+        const { destination, starts_at, ends_at, owner_name, owner_email, emails_to_invite } = request.body
 
         if (dayjs(starts_at).isBefore(new Date())) {
             throw new Error('invalid trip start date')
@@ -30,14 +30,33 @@ export async function createTrip(app: FastifyInstance) {
             throw new Error('invalid trip end date')
         }
 
+
+
+
         const trip = await prisma.trip.create({
             data: {
                 destination,
                 starts_at,
-                ends_at
+                ends_at,
+                participants: {
+                    createMany: {
+                        data: [
+                            {
+                                name: owner_name,
+                                email: owner_email,
+                                is_owner: true,
+                                is_confirmed: true
+                            },
+                            ...emails_to_invite.map(email => {
+                                return { email }
+                            })
+                        ]
+                    }
+                }
             }
         })
 
+        //Envio de E-mail
         const mail = await getMailClient()
 
         const message = await mail.sendMail({
